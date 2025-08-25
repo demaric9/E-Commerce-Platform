8112e98e5a6731d0aa046355551721136d753630
demaric9 <vduy6921@gmail.com>
2025-08-25 19:22:53 +0700

    adding incremental load for facts

--- ecommerce_dbt/models/mart/fact_order_items.sql
+++ ecommerce_dbt/models/mart/fact_order_items.sql
@@ -1,4 +1,9 @@
-{{ config(materialized='table', tags=['mart']) }}
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'order_item_key',
+    incremental_strategy = 'delete+insert',
+    tags=['mart']
+) }}
 
 with int_order_it as (
     select * from {{ ref('int_orders_items') }}
@@ -10,9 +15,9 @@ select
     d.order_key,
     order_id,
     item_number,
-     {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_key,
-     {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_key,
-     {{ dbt_utils.generate_surrogate_key(['seller_id']) }} as seller_key,
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_key,
+    {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_key,
+    {{ dbt_utils.generate_surrogate_key(['seller_id']) }} as seller_key,
     order_status,
     product_price,
     shipping_fee,
@@ -23,3 +28,7 @@ select
     delivery_status_check
 from int_order_it
 join {{ ref('dim_order') }} d using (order_id)
+
+{% if is_incremental() %}
+  where order_date > (select max(order_date) from {{ this }})
+{% endif %}
--- ecommerce_dbt/models/mart/fact_order_reviews.sql
+++ ecommerce_dbt/models/mart/fact_order_reviews.sql
@@ -1,4 +1,9 @@
-{{ config(materialized='table', tags=['mart']) }}
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'review_id',
+    incremental_strategy = 'delete+insert',
+    tags=['mart']
+) }}
 
 with int_order_reviews as (
     select * from {{ ref('int_reviews') }}
@@ -12,3 +17,7 @@ select
     review_creation_date
 from int_order_reviews
 join {{ ref('dim_order') }} d using (order_id)
+
+{% if is_incremental() %}
+  where review_creation_date > (select max(review_creation_date) from {{ this }})
+{% endif %}
